9f9df008744dc2c5451b611192d26dfc9fc2450e
Samuel Forrest <sam@samuelforrest.me>
2026-02-16 14:56:38 +0000

    changed sitemap priority and change frequency

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,16 +7,22 @@ export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
     lastModified: post.metadata.publishedAt,
+    changeFrequency: "weekly" as const,
+    priority: 0.7,
   }));
 
   let projects = getProjects().map((project) => ({
     url: `${baseUrl}/projects/${project.slug}`,
     lastModified: project.metadata.publishedAt,
+    changeFrequency: "weekly" as const,
+    priority: 0.7,
   }));
 
   let routes = ["", "/blog", "/projects"].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
+    changeFrequency: "weekly" as const,
+    priority: route === "" ? 1.0 : 0.8,
   }));
 
   return [...routes, ...blogs, ...projects];
