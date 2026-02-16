05192cb7acc99aa5509467689cef8f5a5658b5ec
Samuel Forrest <sam@samuelforrest.me>
2026-02-16 16:13:29 +0000

    hide documents on mobile

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -27,11 +27,14 @@ export function Navbar() {
         >
           <div className="flex flex-row space-x-0 pr-10">
             {Object.entries(navItems).map(([path, { name }]) => {
+              const isDocuments = path === "/documents";
               return (
                 <Link
                   key={path}
                   href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                  className={`transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1 ${
+                    isDocuments ? "hidden md:flex" : ""
+                  }`}
                 >
                   {name}
                 </Link>
